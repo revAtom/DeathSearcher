4ec7df6b0b150b31f84c2f7fbde992f6c82c994d
Liubomyyr <liubko81@gmail.com>
2020-05-16 14:38:47 +0300

    current project

---
+++ Assets/Scripts/Eviroment/EviromentInteract.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EviromentInteract : MonoBehaviour
+{
+   private Animator anim;
+
+     void OnTriggerEnter2D(Collider2D collision)
+    {
+     anim = collision.gameObject.GetComponent<Animator>();
+
+       if(collision.gameObject.tag == "Player")
+        {
+            anim.SetBool("interact", true);
+        } 
+    }
+}
